cc1559a1a06111344e76fb3a96c4a0a1989ad8df
Chanuka Gishen Mendis <76777641+Chanuka-Gishen@users.noreply.github.com>
2025-05-27 10:41:57 +0530

    feat(): added income payment functionality added

---
+++ src/schema/add-income-schema.js
@@ -0,0 +1,36 @@
+import * as Yup from 'yup';
+
+import { PAY_SC_INCOME } from 'src/constants/payment-source';
+import {
+  PAY_METHOD_BACK_TRN,
+  PAY_METHOD_CARD,
+  PAY_METHOD_CASH,
+  PAY_METHOD_CHEQUE,
+} from 'src/constants/payment-methods';
+
+export const incomePaymentSchema = Yup.object().shape({
+  paymentAmount: Yup.number()
+    .min(0.1, 'Amount should be valid')
+    .required('Amount is required')
+    .typeError('Amount must be a number'),
+
+  paymentSource: Yup.string()
+    .oneOf(PAY_SC_INCOME, 'Invalid payment source')
+    .required('Payment source required'),
+
+  paymentMethod: Yup.string()
+    .oneOf(
+      [PAY_METHOD_CASH, PAY_METHOD_CARD, PAY_METHOD_BACK_TRN, PAY_METHOD_CHEQUE],
+      'Invalid payment method'
+    )
+    .required('Payment method required'),
+
+  paymentTransactionId: Yup.string().nullable().notRequired(),
+
+  paymentNotes: Yup.string().nullable().notRequired(),
+
+  paymentDate: Yup.date()
+    .required('Payment date required')
+    .max(new Date(), 'Payment date cannot be in the future')
+    .typeError('Invalid date format'),
+});
